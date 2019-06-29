b82424d5a00644d46e08e43fa2765cf18068cde7
Александр Рогожин <rogozh@MacBook-Pro.local>
2019-06-29 11:25:40 +0300

    Fix

--- create_db.py
+++ create_db.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from webapp import db, create_app
 
 db.create_all(app=create_app())
-=======
-from  ria_parser import db, create_app
-
-db.create_all(app=create_app())
->>>>>>> origin/nick
